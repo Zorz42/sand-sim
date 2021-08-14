1ebed393fdcc2f083f8b096a5e8ae9ab9e8b0427
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-08-14 17:56:59 +0200

    did some stuff

--- SandSimulation/materialClass.hpp
+++ SandSimulation/materialClass.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
-class particle{
+#include <vector>
+
+struct particle {
     float speed_x;
     float speed_y;
     int timer;
-}
+};
 
-class material{
-    vector color;//this will probably get changed from vector
-    short constant_force
-}
+struct material {
+    std::vector<unsigned char> color;//this will probably get changed from vector
+    short constant_force;
+};
 
-class sand:material{
-    constant_force = 10;
-    color = {237, 205, 88};
+class sand:material {
+public:
+    sand() {
+        constant_force = 10;
+        color = {237, 205, 88};
+    }
     void update();
 };
