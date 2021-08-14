369ad1405ec51f6df425015d2f5eb89c8d664f09
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-08-14 17:51:34 +0200

    switched to sfml

--- SandSimulation/main.cpp
+++ SandSimulation/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-#include <SDL2/SDL.h>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    SDL_Init(SDL_INIT_EVERYTHING);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Sand Simulation");
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear({255, 255, 255});
+        
+        window.display();
+    }
+    
     return 0;
 }
