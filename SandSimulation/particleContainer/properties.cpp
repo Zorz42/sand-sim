e24e717d6c2db2a639a88c2e3d939d345b7e5678
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-08-18 09:41:59 +0200

    improved visuals

--- SandSimulation/particleContainer/properties.cpp
+++ SandSimulation/particleContainer/properties.cpp
@@ -392,16 +392,16 @@ void acidUpdate(ParticleContainer* container, int x, int y, bool even) {
 
 void initMaterials() {
     Materials::air = Material({{90, 90, 90}}, 0, 0);
-    Materials::sand = Material({{237, 205, 88}}, 0.08, 80, &sandUpdate);
-    Materials::water = Material({{52, 145, 173}}, 0.08, 80, &waterUpdate);
-    Materials::wood = Material({{150, 111, 51}, {166, 110, 7}, {158, 108, 25}}, 0, 0);
-    Materials::fire = Material({{222, 91, 16}, {252, 115, 3}, {214, 36, 0}, {255, 215, 15}, {255, 171, 15}}, 0, 20, &fireUpdate);
-    Materials::stone = Material({{133, 133, 133}}, 0, 0);
+    Materials::sand = Material({{245, 210, 92}, {237, 205, 88}, {239, 207, 90}, {235, 203, 86}, {230, 200, 84}}, 0.08, 80, &sandUpdate);
+    Materials::water = Material({{52, 145, 173}, {53, 147, 176}, {47, 140, 169}}, 0.08, 80, &waterUpdate);
+    Materials::wood = Material({{155, 116, 54}, {154, 115, 53}, {156, 117, 55}, {150, 111, 51}}, 0, 0);
+    Materials::fire = Material({{222, 70, 12}, {222, 120, 16}, {222, 125, 16}, {222, 127, 16}, {222, 130, 16}, {222, 182, 16}}, 0, 20, &fireUpdate);
+    Materials::stone = Material({{133, 133, 133}, {135, 135, 135}, {131, 131, 131}}, 0, 0);
     Materials::smoke = Material({{45, 45, 45}, {75, 75, 75}, {105, 105, 105}, {135, 135, 135}, {165, 165, 165}, {195, 195, 195}, {225, 225, 225}}, 0.08, 40, &smokeUpdate);
     Materials::gunpowder = Material({{36, 36, 36}}, 0.08, 40, &sandUpdate);
-    Materials::acid = Material({{31, 255, 61}}, 0.08, 40, &acidUpdate);
-    Materials::americaWantsIt = Material({{10, 10, 10}}, 0.08, 40, &americaWantsItUpdate);
-    Materials::lava = Material({{222, 91, 16}, {252, 115, 3}, {214, 36, 0}, {255, 215, 15}, {255, 171, 15}}, 0.08, 40, &lavaUpdate);
+    Materials::acid = Material({{31, 255, 61}, {32, 255, 62}, {31, 255, 61}}, 0.08, 40, &acidUpdate);
+    Materials::americaWantsIt = Material({{10, 10, 10}, {12, 12, 12}, {9, 9, 9}}, 0.08, 40, &americaWantsItUpdate);
+    Materials::lava = Material({{252, 91, 16}, {252, 95, 16}, {252, 97, 16}, {252, 102, 16}, {252, 142, 16}}, 0.08, 40, &lavaUpdate);
 }
 
 void swapParticles(Particle& particle1, Particle& particle2) {
